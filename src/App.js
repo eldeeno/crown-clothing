8db9529b8f64d06f948b160633a2f1fc4b6a5681
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2020-08-07 17:09:34 +0100

    Google signin 02

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/hompage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInUp from './pages/sign-in-up/sign-in-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInUp} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
