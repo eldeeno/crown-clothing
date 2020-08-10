e44ad9b008fad47bc5d881ae4ced3eebe7b80d30
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2020-08-10 16:38:45 +0100

    Completed signin with firebase feature

---
+++ src/firebase/test.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('RGfPZWamajarIFLFCwxV').collection('cartItems').doc('q6qIU2rokrmOmvitN5MD')
+firestore.doc('/user/RGfPZWamajarIFLFCwxV/cartItems/q6qIU2rokrmOmvitN5MD');
+firestore.collection('/user/RGfPZWamajarIFLFCwxV/cartItems');
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route, NavLink, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.render(
